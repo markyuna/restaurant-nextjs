089faec91bcd84817b9aa244426b22deac2d4b0c
MARCOS <marcossuarezr88@gmail.com>
2024-05-13 21:31:07 +0200

    restaurant-app build good

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+          test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'assets/media/' // directorio de salida para los archivos de video
+              }
+            }
+          ]
+        });
+    
+        return config;
+      }
+};
 
 export default nextConfig;
